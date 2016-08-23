17f3bc532eeffa0cee34b496d199b9c68580444b
Eddie Jaoude <eddie@jaoudestudios.com>
2016-08-23 07:59:02 +0100

    [INCOMPLETE] Dialog/Overlay template added but not working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,37 @@
  * Controller of the werocktechApp
  */
 angular.module('werocktechApp')
-  .controller('MainCtrl', function (series) {
+  .controller('MainCtrl', function (series, $mdDialog) {
     var self = this;
 
-    this.filterByTag = function(keyword) {
+    self.filterByTag = function(keyword) {
       self.search = {
         'query': keyword
       }
     };
 
-    this.getSeries = function() {
+    self.getSeries = function() {
       series.findAll().then(function successCallback(response) {
         self.series = response.data;
       }, function errorCallback(response) {
         console.log(response.data);
       });
-    }
+    };
+
+    self.search = function(ev) {
+      $mdDialog.show({
+        controller: self,
+        templateUrl: '/views/template/search.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        fullscreen: self.customFullscreen // Only for -xs, -sm breakpoints.
+      });
+        // .then(function (answer) {
+        //   self.status = 'You said the information was "' + answer + '".';
+        // }, function () {
+        //   self.status = 'You cancelled the dialog.';
+        // });
+    };
+
   });
